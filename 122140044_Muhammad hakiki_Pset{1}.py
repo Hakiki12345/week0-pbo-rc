9a49d42b0b900756f45410e0d3955ec194f6ac36
Hakiki12345 <145894106+Hakiki12345@users.noreply.github.com>
2024-02-26 23:24:22 +0700

    Add files via upload

--- 122140044_Muhammad hakiki_Pset{1}.py
+++ 122140044_Muhammad hakiki_Pset{1}.py
@@ -1,5 +1,17 @@
-tinggi = int(input('Masukkan tinggi piramida yang  anda inginkan:'))
+class PiramidaGenerator:
+    def __init__(self):
+        self.tinggi = 0
 
-for i in range(1, tinggi+1):
-    
-    print((tinggi - i) * ' ' + (2 * i - 1) * '*')
+    def atur_tinggi(self, tinggi):
+        self.tinggi = tinggi
+
+    def buat_piramida(self):
+        for i in range(1, self.tinggi+1):
+            print((self.tinggi - i) * ' ' + (2 * i - 1) * '*')
+
+# Logika main untuk mengakses kelas dan membuat objek PiramidaGenerator
+if __name__ == "__main__":
+    generator = PiramidaGenerator()
+    tinggi_piramida = int(input('Ketik tinggi piramida yang Anda inginkan: '))
+    generator.atur_tinggi(tinggi_piramida)
+    generator.buat_piramida()
